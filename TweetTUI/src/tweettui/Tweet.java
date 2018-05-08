90f77a5202f3227d0a77076686454b0afa3d9f7f
saguywalker <guyguy252@gmail.com>
2018-05-08 22:08:59 +0700

    delete date attribute

--- TweetTUI/src/tweettui/Tweet.java
+++ TweetTUI/src/tweettui/Tweet.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package tweettui;
 
 public class Tweet {
@@ -37,3 +38,44 @@ public class Tweet {
     }
 
 }
+=======
+package tweettui;
+
+public class Tweet {
+
+    private String msg;
+    private String owner;
+    private String date;
+
+    public Tweet() {
+    }
+
+    public Tweet(String msg) {
+        this.msg = msg;
+    }
+
+    public Tweet(String msg, String owner, String date) {
+        this.msg = msg;
+        this.owner = owner;
+        this.date = date;
+    }
+
+    public Tweet(String msg, String owner) {
+        this.msg = msg;
+        this.owner = owner;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+}
+>>>>>>> origin/master
