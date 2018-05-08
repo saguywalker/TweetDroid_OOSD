c56c4dd410efccb344f72239c24a41e5abdd2a55
saguywalker <guyguy252@gmail.com>
2018-05-08 22:11:52 +0700

    fix Tweet class

--- TweetTUI/src/tweettui/Tweet.java
+++ TweetTUI/src/tweettui/Tweet.java
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 package tweettui;
 
 public class Tweet {
 
     private String msg;
     private String owner;
-    private String date;
 
     public Tweet() {
-    }
-
-    public Tweet(String msg) {
-        this.msg = msg;
-    }
-
-    public Tweet(String msg, String owner, String date) {
-        this.msg = msg;
-        this.owner = owner;
-        this.date = date;
+        this.msg = null;
+        this.owner = null;
     }
 
     public Tweet(String msg, String owner) {
@@ -33,49 +23,4 @@ public class Tweet {
         return owner;
     }
 
-    public String getDate() {
-        return date;
-    }
-
 }
-=======
-package tweettui;
-
-public class Tweet {
-
-    private String msg;
-    private String owner;
-    private String date;
-
-    public Tweet() {
-    }
-
-    public Tweet(String msg) {
-        this.msg = msg;
-    }
-
-    public Tweet(String msg, String owner, String date) {
-        this.msg = msg;
-        this.owner = owner;
-        this.date = date;
-    }
-
-    public Tweet(String msg, String owner) {
-        this.msg = msg;
-        this.owner = owner;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-}
->>>>>>> origin/master
